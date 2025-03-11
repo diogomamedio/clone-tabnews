6e264be73967c72f0604b2ff4ee0180adc744210
Diogo Marcelo Mamédio <56289867+diogomamedio@users.noreply.github.com>
2025-03-11 14:05:20 +0000

    Make the database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -2,6 +2,7 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
+    // Usa dotenv para pegar essas variáveis de ambiente
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DATABASE,
@@ -9,9 +10,15 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  //sem o Try... toda vez que uma consulta der errado ficará uma conexão aberta.
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,40 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Tudo Ok na comunicação com o servidor" });
+  // retorna a data atual em ISO8601
+  const updatedAt = new Date().toISOString();
+
+  // Retorna a versão do banco
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  // Retorna atividades no banco postgres
+  const databaseName = process.env.POSTGRES_DB;
+
+  // Usando a sanatizaçao de banco do node-postgres
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count; // número de conexões abertas no postgres
+
+  response.status(200).json({
+    status: "ok",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
