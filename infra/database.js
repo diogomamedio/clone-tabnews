ddfdeedb14309a50f35eb218836e14e9a1293750
Diogo Marcelo Mamédio <56289867+diogomamedio@users.noreply.github.com>
2025-03-12 10:53:29 +0000

    Fix error connection DB

--- infra/database.js
+++ infra/database.js
@@ -5,11 +5,18 @@ async function query(queryObject) {
     // Usa dotenv para pegar essas variáveis de ambiente
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
   });
   await client.connect();
+  console.log("Credencias do Postgres: ", {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
   //sem o Try... toda vez que uma consulta der errado ficará uma conexão aberta.
   try {
     const result = await client.query(queryObject);
